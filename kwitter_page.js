23862e88e540f6491468629940a6a5d833563650
byjucoding <95137527+byjucoding@users.noreply.github.com>
2022-03-06 11:55:33 -0500

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCivf5i5HIkqzHzj_QK-4KKLaEuaptv_O0",
+    authDomain: "kwitter-app-397f1.firebaseapp.com",
+    databaseURL: "https://kwitter-app-397f1-default-rtdb.firebaseio.com",
+    projectId: "kwitter-app-397f1",
+    storageBucket: "kwitter-app-397f1.appspot.com",
+    messagingSenderId: "386150458946",
+    appId: "1:386150458946:web:38120b0a65c9c5a6cf2fdb",
+    measurementId: "G-RWLSGJ5YNJ"
+  };
+  
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
